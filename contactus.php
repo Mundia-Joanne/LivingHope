b7c30d24abce1c5006a3e7a56e092be62530e932
Joanne-Mundia <36207969+Joanne-Mundia@users.noreply.github.com>
2021-07-05 06:06:16 +0300

    Initial commit

---
+++ contactus.php
@@ -0,0 +1,14 @@
+<!-- <?php 
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+
+	$to = "joannemundia@gmail.com";
+	$subject = "Message incoming";
+	$body = "This is an automated message.Please dont reply to this email .";
+
+	mail($to, $subject, $body);
+
+	echo "Message sent" ;
+ ?> -->
